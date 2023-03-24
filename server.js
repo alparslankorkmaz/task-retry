14ec7b05a92faf73d505d616175cd400e7ebe3b1
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-03-24 12:51:17 +0000

    server side validation fix

--- server.js
+++ server.js
@@ -14,18 +14,17 @@ const Participant = require("./models/participant");
 dbUrl = process.env.DB_URL;
 mongoose.connect(dbUrl);
 
-const bodyParser = require("body-parser");
 const app = express();
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.engine("ejs", ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
 app.use(mongoSanitize());
 
+// helmet bypass
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
   "https://cdnjs.cloudflare.com/",
@@ -54,15 +53,27 @@ app.use(
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.static(path.join(__dirname, "/src")));
 
-const formSchema = Joi.object({
-  firstname: Joi.string().max(50).required(),
-  lastname: Joi.string().max(50).required(),
-  companyname: Joi.string().max(50).required(),
-  email: Joi.string().max(50).required(),
-  address: Joi.string().max(50).required(),
-  postcode: Joi.string().max(20).required(),
-}).required();
-formSchema.validate();
+// server side validation
+const validateReg = (req, res, next) => {
+  const formSchema = Joi.object({
+    firstname: Joi.string().max(50).required(),
+    lastname: Joi.string().max(50).required(),
+    companyname: Joi.string().max(50).required(),
+    email: Joi.string().email().lowercase().max(50).required(),
+    address: Joi.string().max(50).required(),
+    postcode: Joi.string().max(20).required(),
+  }).required();
+
+  const newReg = req.body;
+
+  const { error } = formSchema.validate(newReg);
+
+  if (error) {
+    return res.send("Invalid request.");
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   res.render(__dirname + "/views/index.ejs");
@@ -72,28 +83,22 @@ app.get("/success", (req, res) => {
   res.render(__dirname + "/views/success.ejs");
 });
 
-app.post("/register", urlencodedParser, async (req, res, next) => {
-  const { error, value } = formSchema.validate(req.body);
-
-  if (error) {
-    return res.send("Invalid Request.");
-  } else {
-    try {
-      const newPar = new Participant({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        companyname: req.body.companyname,
-        email: req.body.email,
-        address: req.body.address,
-        postcode: req.body.postcode,
-      });
-      await newPar.save();
-      setTimeout(function () {
-        res.redirect("/success");
-      }, 2000);
-    } catch (e) {
-      next(e);
-    }
+app.post("/register", validateReg, async (req, res, next) => {
+  try {
+    const newPar = new Participant({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      companyname: req.body.companyname,
+      email: req.body.email,
+      address: req.body.address,
+      postcode: req.body.postcode,
+    });
+    await newPar.save();
+    setTimeout(function () {
+      res.redirect("/success");
+    }, 2000);
+  } catch {
+    next();
   }
 });
 
