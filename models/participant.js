1171dbf65d79343f07ed6c2dde1d4ea6b7ec8cd1
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-03-26 17:54:01 +0100

    participant model timestamp added

--- models/participant.js
+++ models/participant.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ParticipantSchema = new Schema({
-  firstname: String,
-  lastname: String,
-  companyname: String,
-  email: String,
-  address: String,
-  postcode: String,
-});
+const ParticipantSchema = new Schema(
+  {
+    firstname: { String, required: true },
+    lastname: { String, required: true },
+    companyname: { String, required: true },
+    email: { String, required: true },
+    address: { String, required: true },
+    postcode: { String, required: true },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Participant", ParticipantSchema);
